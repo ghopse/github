143a42e71e2d99f9c2e8e507d415912612ea5e96
lisaintaipei <96688092+lisaintaipei@users.noreply.github.com>
2022-02-19 22:23:35 +0800

    Lisa's version

--- index.php
+++ index.php
@@ -12,25 +12,156 @@
 		<title>Car Parking Navigation System</title>
 	</head>
 	<body>
-<!-- 		<div id="splash">
-			<img src="img/logo.png" alt="" class="logo">
-		</div> -->
-		<div class="content">
-			<header class="app-header">
-				<div class="container">
-					<p>Pick your action</p>
-				</div>
-			</header>
-			<div class="grid-container">
-				<div class="option">
-					<img src="img/car_parking2.svg" alt="" class="logo">
-					<p>Save parking</p>
-				</div>
-				<div class="option">
-					<img src="img/car_out.svg" alt="" class="logo">
-					<p>GOGOGO To your car</p>
-				</div>
-			</div>
-		</div>
-	</body>
+	<section class="action-screen">
+  <h4>
+    Pick Your Action
+  </h4>
+  <div class="save-parking">
+    <div class="icon-1">
+      icon
+    </div>
+    <h3 class="text-1">
+      Save parking
+    </h3>
+  </div>
+  <div class="to-your-car">
+    <div class="icon-2">
+      icon
+    </div>
+    <h3 class="text-2">
+      To your car
+    </h3>
+  </div>
+</section>
+<section class="save-parking-input-1">
+  <header class="to-action-screen-1">
+    icon
+  </header>
+  <div class="illustration-icon">
+    <div class="icon-3">
+      icon
+    </div>
+    <h4>
+      Please enter your car plate number
+    </h4>
+  </div>
+  <div class="input-section">
+    <h5 class="text-3">  
+        Car plate number
+    </h5>
+      <input type="text" placeholder="EX. AB1234">
+      <button id="btn-1">
+        Next
+      </button>
+    </div>
+</section>
+<section class="save-parking-input-2">
+  <header class="to-input-1">
+    icon
+  </header>
+  <div class="illustration-icon">
+    <div class="icon-4">
+      icon
+    </div>
+    <h4>
+      Please enter parking space
+    </h4>
+  </div>
+  <div class="input-section">
+      <h5 class="text-4">
+        Parking space
+      </h5>
+      <input type="text" placeholder="EX. B1A01">
+      <button id="btn-2">
+        Save now
+      </button>
+    </div>
+  <div class="previous-step">
+    <h4>
+      Previous step
+    </h4>
+  </div>
+</section>
+<section class="save-parking-action">
+   <header class="to-input-2">
+    icon
+  </header>
+  <div class="plate-no-info">
+    <h4>Your car plate number:</h4>
+    <h3>ABC5678</h3>
+  </div>
+  <div class="parking-space-info">
+    <h4>Your parking space:</h4>
+    <h3>B3A15</h3>
+  </div>
+  <div class="action-menu">
+    <div class="find-exit">
+       <div class="icon-5">
+      icon
+    </div>
+    <h4 class="text-5">
+      Find the nearest exit
+    </h4>
+    </div>
+    <div class="back-to-car">
+      <div class="icon-6">
+      icon
+    </div>
+    <h4 class="text-6">
+      Way to my car
+    </h4>
+    </div>
+  </div>
+</section>
+<section class="map-to-exit"></section>
+<section class="retrieve-parking-input-1">
+  <header class="to-action-screen-2">
+    icon
+  </header>
+  <div class="illustration-icon">
+    <div class="icon-7">
+      icon
+    </div>
+    <h4>
+      Please enter your car plate number
+    </h4>
+  </div>
+  <div class="input-section">
+    <h5 class="text-7">  
+        Car plate number
+    </h5>
+      <input type="text" placeholder="EX. AB1234">
+      <button id="btn-3">
+        Next
+      </button>
+    </div>
+</section>
+<section class="retrieve-parking-input-2">
+   <header class="to-retrieve-input-1">
+    icon
+  </header>
+  <div class="illustration-icon">
+    <div class="icon-8">
+      icon
+    </div>
+    <h4>
+      Please enter your location
+    </h4>
+  </div>
+  <div class="input-section">
+      <h5 class="text-8">
+        Which exit are you at now?
+      </h5>
+      <input type="text" placeholder="EX. B1A01">
+      <button id="btn-4">
+        To my car
+      </button>
+    </div>
+  <div class="previous-step-2">
+    <h4>
+      Previous step
+    </h4>
+  </div>
+</section>
+</body>
 </html>
