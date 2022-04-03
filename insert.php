3f9e354b846912a330f330936dff4f0b745785a0
ghopse <ghopse.server@hotmail.com>
2022-04-03 17:01:08 +0800

    DB Connection test

--- insert.php
+++ insert.php
@@ -4,33 +4,12 @@ $dbUsername = "carpark";
 $dbPassword = "C@rpark123";
 $dbName = "carpark_db";
 
-$plateNo = $_POST['plateNo'];
-$timeIn = $_POST['timeIn'];
-$timeOut = $_POST['timeOut'];
-$slotNo = $_POST['slotNo'];
-
 $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
 
-if(isset($_POST['submit'])){
+if($conn->connect_error) { die('Error'.('.$mysqli->connect_errno.')'.$conn->connect_error');
+}else{
 
-    if(!empty($plateNo)){
-
-        $query = "insert into test(plateNo) values('$plateNo')" ;
-
-        $run = mysqli_query($conn,$query) or die(mysqli_error());
-
-        if($run){
-            echo " Plate Number recorded" ;
-        }
-        else{
-
-            echo "Failed to record"
-        }
-
-    }
-    else{
-        echo "Please fill your vehicle plate number" ;
-    }
- 
+    echo "Connected to database";
 }
+
 ?>
