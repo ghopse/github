b50ab5264c205cc4c1ca50b6258b40e27b613d67
ghopse <ghopse.server@hotmail.com>
2022-04-06 22:40:46 +0800

    Simple DB connection test - SK

--- insert.php
+++ insert.php
@@ -1,11 +1,9 @@
 <?php
+$mysqli = new mysqli("localhost","ghopse","aaAA@@44..!!","carpark_db");
 
-$mysqli = new mysqli('localhost', 'ghopse', 'aaAA@@44..!!', 'carpark_db');
-
-if($mysqli->connect_error) { die('Error'.('.$mysqli->connect_errno.')'.$mysqli->connect_error');
-}else{
-
-    echo "Connected to database";
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
 }
-
 ?>
