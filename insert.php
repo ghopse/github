1794de28941ad05da59125ba3f2bd8ebfa4feec6
ghopse <ghopse.server@hotmail.com>
2022-04-10 00:59:26 +0800

    Test insert h3 between php - SK

--- insert.php
+++ insert.php
@@ -8,8 +8,8 @@
                 </a>
             </div>
             <div class="plate-no-info">
-                <h4>Your car plate number:</h4><br>
-                <?php echo $_POST["plateNo"]; ?>
+                <h4>Your car plate number:</h4>
+                <h3><?php echo $_POST["plateNo"]; ?></h3>
             </div>
             <div class="find-exit">
                 <a href="index.html#map-to-exit"><button id="btn-3">Find the nearest exit</button></a>
