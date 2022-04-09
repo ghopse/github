7fce72624fd6efd7f13c623e52d4dcfb0bea9370
ghopse <ghopse.server@hotmail.com>
2022-04-10 00:51:08 +0800

    Update a href link - SK

--- insert.php
+++ insert.php
@@ -3,7 +3,7 @@
     <body>
         <section id="save-parking-action">
             <div class="to-input-2">
-                <a href="#save-parking-input-2">
+                <a href="index.html#save-parking-input-2">
                     <img class="go-back" src="img/previous_page.svg" atl="">
                 </a>
             </div>
@@ -12,7 +12,7 @@
                 <?php echo $_POST["plateNo"]; ?>
             </div>
             <div class="find-exit">
-                <a href="#map-to-exit"><button id="btn-3">Find the nearest exit</button></a>
+                <a href="index.html#map-to-exit"><button id="btn-3">Find the nearest exit</button></a>
             </div>
         </section>
     </body>
