3da29c7c51ca0ed85b20f0e9d892fabd30076a94
ghopse <ghopse.server@hotmail.com>
2022-05-07 03:38:39 +0800

    fix 500 errors? - SK

--- retrieve.php
+++ retrieve.php
@@ -34,7 +34,7 @@
                         die("Connection failed: " . mysqli_connect_error());
                     }
 
-                    // Query data
+                    /* Query data
                     $sql = "SELECT * FROM test2 WHERE plateNo = $_POST["plateNo"]";
                     $result = mysqli_query($conn, $sql);
                     $resultCheck = mysqli_num_rows($result);
@@ -42,9 +42,10 @@
                     if ($resultcheck > 0) {
                         while ($row = mysqli_fetch_assoc($result)) {
                             echo $row['slotNo']
-                        }
+                            
+                        } 
                     }
-
+*/
                     // Close connection
                     mysqli_close($conn);
                     ?>
