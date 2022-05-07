80a549890c1b8660622ce8e4ecbefb00b4df6ea2
lisaintaipei <96688092+lisaintaipei@users.noreply.github.com>
2022-05-07 22:25:24 +0800

    update 5/7_11

--- js/general.js
+++ js/general.js
@@ -84,6 +84,23 @@ $(function(){
 
 });
 
+$(function() {
+  var availableSlots = [
+    "A1",
+    "A2",
+    "A3",
+    "B1",
+    "B2",
+    "B3",
+    "C1",
+    "C2",
+    "C3"
+  ];
+  $( "#input2" ).autocomplete({
+    source: availableSlots
+  });
+});
+
 });
 
 
